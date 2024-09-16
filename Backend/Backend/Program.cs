7a18b0fac40151990086adb77b71f4d9ad987780
SMB-M87 <ben.wijtmans@windesheim.nl>
2024-09-16 04:59:45 +0200

    Program.cs subscribed

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,10 +1,16 @@
+using Backend.Controllers;
 using Backend.Models;
 using Backend.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddTransient<IGameRepository, GameRepository>();
+builder.Services.AddTransient<IRepository, Repository>()
+    .AddTransient<IGameRepository, GameRepository>()
+    .AddTransient<IResultRepository, ResultRepository>()
+    .AddTransient<IPlayerRepository, PlayerRepository>()
+    .AddTransient<GameController>()
+    .AddTransient<ResultController>()
+    .AddTransient<PlayerController>();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
