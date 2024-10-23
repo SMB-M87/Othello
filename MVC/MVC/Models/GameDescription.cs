4f8f9f945f7ffdf0c25def85ab3b92e42904073e
SMB-M87 <ben.wijtmans@windesheim.nl>
2024-10-23 22:59:58 +0200

    Cleaning

--- MVC/MVC/Models/GameDescription.cs
+++ MVC/MVC/Models/GameDescription.cs
@@ -2,7 +2,6 @@
 {
     public class GameDescription
     {
-        public string Token { get; set; } = null!;
         public string Description { get; set; } = null!;
         public string Player { get; set; } = null!;
         public string Stats { get; set; } = null!;
