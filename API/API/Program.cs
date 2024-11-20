eb271ee7ce7f6c71753ce6a17f3828947c20e915
SMB-M87 <ben.wijtmans@windesheim.nl>
2024-11-20 18:27:14 +0100

    Fix: API Allow Any Origin Programs.cs

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -61,7 +61,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", builder =>
     {
-        builder.WithOrigins("http://127.0.0.1:5501")
+        builder.AllowAnyOrigin()
                .AllowAnyHeader()
                .AllowAnyMethod();
     });
